6cc2333f09274395b6db470158b3563cde194e8f
Valeri Shurgaia <valerishurgo@gmail.com>
2023-12-27 17:53:08 +0400

    fix scroll bug

--- common/components/__molecules/MainPageThree/MainPageThree.tsx
+++ common/components/__molecules/MainPageThree/MainPageThree.tsx
@@ -6,7 +6,10 @@ import Link from "next/link";
 
 const MainPageThree = () => {
   return (
-    <section className="xl:h-screen  flex-col px-10 justify-center lg:pt-20 pt-10  relative">
+    <section
+      className="xl:h-screen  flex-col px-10 justify-center lg:pt-20 pt-10  relative"
+      id="theTradition"
+    >
       <GoldOvalLine className="absolute w-full  top-0 3xl:top-32 left-0  lg:top-20 " />
       <div className="hidden lg:flex justify-between gap-7 ">
         <h3 className="3xl:text-[80px]  text-6xl 3xl:pl-32 3xl:pt-10 pt-6 pl-16 font-bold text-white flex-1 block">
